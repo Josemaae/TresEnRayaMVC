b91471469895fe97c4cfabcc9745b10e1ec2ea00
José Manuel Alfonso Espino <josemaae96ah@gmail.com>
2021-03-22 20:59:05 +0100

    .

---
+++ src/modelo/Casillas.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Casillas {
+	
+	private Valor valor;
+	
+	public enum Valor{
+		X,O;
+	}
+	
+}
---
+++ src/modelo/Tablero.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Tablero {
+	
+	private Casillas[] casillas;
+	
+}
