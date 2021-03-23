028b5e4c98e09fdf1b8443596ea597be7a92ac0b
José Manuel Alfonso Espino <josemaae96ah@gmail.com>
2021-03-23 12:15:56 +0100

    .

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -1,16 +1,7 @@
 package modelo;
 
 public class Juego {
-	
-//	Modelo de información
-//	Número de jugadores (juego)
-//	Tipos de ficha (juego)
-//	Numero de fichas (juego)
-//	Casillas de un tablero (Tablero). Cantidad de casillas, filas x columnas, 
-//	Tablero es un conjunto de casillas.
-//	Casilla (Tablero) sabemos si está ocupada, o no, por alguna ficha
-//	Posición de cada ficha de cada jugador (innecesario)
-	
+
 	private int numeroJugadores=2;
 	private Valor tipoFicha;
 	private int numeroDeFichas=3;
